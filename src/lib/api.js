0db3d90dcffafde0149f8448cdaf2ae0a0f0abdf
Akhil Varma Gadiraju <akhilgadiraju@gmail.com>
2026-03-10 15:21:45 +0530

    changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,16 @@
+const basicAuth = btoa(
+  `${import.meta.env.VITE_API_USERNAME}:${import.meta.env.VITE_API_PASSWORD}`,
+);
+
 export async function lookupBank(url) {
   const res = await fetch(
     `${import.meta.env.VITE_API_BASE_URL}/webhook/find-ebanking-provider`,
     {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${basicAuth}`,
+      },
       body: JSON.stringify({ bank_url: url }),
     },
   );
